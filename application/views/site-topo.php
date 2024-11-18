867b2da1d6102a7df1425635da161a2f41c01699
Escobarly <callebyamaral@gmail.com>
2024-11-18 10:54:25 -0300

    Pasta Application inteira e os CSS's da pagina no geral e do site topo.

---
+++ application/views/site-topo.php
@@ -0,0 +1,17 @@
+<div class="site-topo modulo">
+    <div class="container">
+        <div class="bloco-1">
+            <a href="#" target="_blank">
+                <img class="logo" src="arquivos/imagens/logo.png">
+            </a>
+            <span class="tituloPadrao1">
+                Olá, Informática 3
+            </span>
+        </div>
+        <div class="bloco-2">
+            <a href="#horario"><span class="tituloPadrao2">Horário</span></a>
+            <a href="#cardapio"><span class="tituloPadrao2 a_selected">Cardápio</span></a>
+            <a href="#ajuda"><span class="tituloPadrao2">Ajuda</span></a>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
-<?php
-include('assets/others/cnn.php')
-?>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filandus</title>
+    <style>
+        <?php require_once('arquivos/css/template.css'); ?>
+        <?php require_once('arquivos/css/site-topo.css'); ?>
+    </style>
+</head>
 <body>
-    <footer>
-        <a src="index.php"><img src="Vector.svg"></a>
-    </footer>
+    <?php require_once('application/views/site-topo.php'); ?>
 </body>
 </html>
