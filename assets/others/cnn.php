81c5d4d3e153440b3cb5bf72380b13122ce0c73e
GabrielCoMont <150257778+GabrielCoMont@users.noreply.github.com>
2024-11-18 10:06:52 -0300

    tirando html do cnn

--- assets/others/cnn.php
+++ assets/others/cnn.php
@@ -1,13 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style.css">
     <title>Filandus</title>
 </head>
 <body>
-<script src="script.js"></script>
-<script src="style.js"></script>
+<script src="./script.js"></script>
+<script src="./style.js"></script>
 </body>
-</html>
